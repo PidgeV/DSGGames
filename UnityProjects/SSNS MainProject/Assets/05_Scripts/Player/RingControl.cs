c934535cad0576abad1dd24b9488539b74099dc5
Nathen Toms <45152858+NTomsNC@users.noreply.github.com>
2019-10-11 20:31:19 -0400

    Main Project
    Srat moving things from the other 3 test projects into the main. It has the HDRP set up.
    
    I broke the ship controller and dont know how to fix it sorry

---
+++ UnityProjects/SSNS MainProject/Assets/05_Scripts/Player/RingControl.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// RingControl.cs
+// Uses the ring in the center to determine how the ship rotates
+[RequireComponent(typeof(RectTransform))]
+public class RingControl : MonoBehaviour
+{
+    Image target; // The dot in the ring
+
+    Vector2 dir; // The direction from the center to the target
+
+    Vector2 center; // The center
+
+    // Radius for the inner and outer rings
+    float ringRadius;
+    float innerRingRadius;
+
+    private void Awake()
+    {
+        target = transform.Find("Target").GetComponent<Image>();
+        center = transform.Find("Redicle").GetComponent<RectTransform>().transform.position;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+        // Gets the size of the image to determine the radius for the outer ring
+        RectTransform rectTransform = GetComponent<RectTransform>();
+
+        if (rectTransform.sizeDelta.x > rectTransform.sizeDelta.y)
+        {
+            ringRadius = rectTransform.sizeDelta.x / 2.0f;
+        }
+        else
+        {
+            ringRadius = rectTransform.sizeDelta.y / 2.0f;
+        }
+
+        // Gets the size of the image to determine the radius for the inner ring
+        RectTransform innerRectTransform = transform.Find("Inner Ring").GetComponent<RectTransform>();
+
+        if (innerRectTransform.sizeDelta.x > innerRectTransform.sizeDelta.y)
+        {
+            innerRingRadius = innerRectTransform.sizeDelta.x / 2.0f;
+        }
+        else
+        {
+            innerRingRadius = innerRectTransform.sizeDelta.y / 2.0f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        // Doesn't update when there's no input or the game isn't focused
+        if (mouseX == 0 && mouseY == 0 || !Application.isFocused) return;
+
+        // Finds the new position for the target
+        Vector2 newPosition = target.transform.position + new Vector3(mouseX, mouseY) * Time.deltaTime * 100;
+
+        dir = newPosition - center;
+
+        // Determines if the target is in the inner ring or is outside of the outer ring
+        Vector3 diff = newPosition - center;
+        float distance = diff.sqrMagnitude;
+
+        if (distance <= innerRingRadius * innerRingRadius)
+        {
+            dir = new Vector2();
+        }
+        else if (distance > ringRadius * ringRadius)
+        {
+            newPosition = center + dir.normalized * ringRadius;
+        }
+
+        // Moves the target
+        target.transform.position = newPosition;
+    }
+
+    // Converts the direction for using with the ship's rotation
+    public Vector2 GetShipRotation { get { return new Vector2(-dir.y, dir.x); } }
+}
---
+++ UnityProjects/SSNS MainProject/Assets/05_Scripts/Player/TestPlayer.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// This class is a protoype for flying mechanics. It uses a velocity system to keep momentum of flight. You will need to take the physics into account when flying.
+/// </summary>
+
+[RequireComponent(typeof(Rigidbody))]
+public class TestPlayer : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    public float currentSpeed = 0;
+    [Space(10)]
+    public float minSpeed = 5f;
+    public float maxSpeed = 50f;
+    public float acceleration = 10f;
+    public float deceleration = 10f;
+    public float spinSpeed = 50f;
+
+    [Range(0.2f, 2.0f)]
+    public float rotateSpeed = 1.0f;
+
+    [Space(10)]
+    public RingControl ringControl;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float accel = Input.GetAxis("Vertical");
+        float rot = Input.GetAxis("SpinAxis");
+
+        //Move forward
+        if (currentSpeed <= maxSpeed && currentSpeed >= minSpeed)
+        {
+            if (accel > 0)
+            {
+                currentSpeed += accel * acceleration * Time.deltaTime;
+            }
+            else
+            {
+                currentSpeed += accel * deceleration * Time.deltaTime;
+            }
+        }
+        else
+        {
+            if (currentSpeed <= minSpeed)
+            {
+                currentSpeed = minSpeed;
+            }
+            else
+            {
+                currentSpeed = maxSpeed;
+            }
+        }
+
+        //Rotation
+        transform.Rotate(new Vector3(0, 0, rot * spinSpeed * Time.deltaTime), Space.Self); //Spinning rotation
+
+        if (ringControl) // Ring rotation
+        {
+            Vector2 ringDir = ringControl.GetShipRotation;
+            transform.Rotate(ringDir * rotateSpeed * Time.deltaTime);
+        }
+
+        rigidbody.velocity = transform.forward * currentSpeed;
+
+        Debug.DrawRay(transform.position, transform.forward * 100, Color.red);
+    }
+
+    private void OnGUI()
+    {
+        //GUI.color = Color.white;
+        //GUI.Label(new Rect(5, 5, 200, 50), "Current Speed = " + currentSpeed);
+    }
+}
