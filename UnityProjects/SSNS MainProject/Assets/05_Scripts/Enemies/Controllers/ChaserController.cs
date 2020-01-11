4624656e71e1600b123f9462169c0d036ac8d0ab
PidgeV <39803542+PidgeV@users.noreply.github.com>
2020-01-10 19:01:34 -0500

    Worked 6.5-7 w Blake

---
+++ UnityProjects/SSNS MainProject/Assets/05_Scripts/Enemies/Controllers/ChaserController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Complete
+{
+    public class ChaserController : AdvancedFSM
+    {
+        [SerializeField] GameObject destroyedPrefab;
+        [SerializeField] GameObject[] waypoints;
+      
+        [Header("Green: forward direction and collision check.")]
+        [Header("Red: intercept calculation.")]
+        [Header("Blue: Velocity and collision check.")]
+        [SerializeField] bool debugDraw = false;
+
+        [SerializeField] float dot;
+
+        [Space(15)]
+        [SerializeField] LayerMask obstacleLayer;
+        [SerializeField] float collisionCheckDistance = 150f;
+
+        [Space(15)]
+        [Tooltip("The acceleration of the ship in meters per second")]
+        [SerializeField] float acceleration = 20;// { get { return acceleration; } }
+        [SerializeField] float chargeAcceleration = 50f;
+        [Tooltip("The speed the ship will rotate")]
+        [SerializeField] float regRotationForce = 2f;
+        [SerializeField] float chargeRotationForce = 2f;
+
+        [Space(15)]
+        [SerializeField] float waypointDistance = 50f;
+        [SerializeField] float playerDistance = 100f;
+        [Tooltip("Time in seconds between trajectory calculations")]
+        [SerializeField] float calculateInterval = 0.5f;
+      
+        Rigidbody rbTarget;
+        Rigidbody rbSelf;
+        Vector3 interceptPoint;
+
+        //Obstacle variables
+        Vector3 obstacleAvoidDirection = Vector3.right;
+        bool obstacleHit = false;
+        float obstacleTimer = 0;
+        [SerializeField] float avoidTime = 2f;
+        [Tooltip("Size of ray for collision checking. Larger numbers will mean the avoidance is larger")]
+        [SerializeField] float raySize = 7.5f;
+
+
+
+        Player player;
+        // Start is called before the first frame update
+        void Start()
+        {
+            player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void ConstructFSM()
+        {
+            DeadState deadState = new DeadState(this, destroyedPrefab);
+            ChaserPatrolState patrol = new ChaserPatrolState(this, player, waypoints, waypointDistance, playerDistance);
+
+            patrol.AddTransition(Transition.NoHealth, FSMStateID.Dead);
+            patrol.AddTransition(Transition.SawPlayer, FSMStateID.Chasing);
+
+            AddFSMState(deadState);
+        }
+    }
+}
---
+++ UnityProjects/SSNS MainProject/Assets/05_Scripts/Enemies/States/ChaserPatrolState.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Complete
+{
+    public class ChaserPatrolState : FSMState
+    {
+        private Player player;
+        private ChaserController controller;
+        private GameObject[] waypoints;
+        private float distance;
+        private float playerDist;
+        private int patrolID = 0;
+        private bool randomPoint;
+        private bool sawPlayer = false;
+        public ChaserPatrolState(ChaserController enemyController, Player playerObj, GameObject[] wayPoints, float waypointDistance, float playerDistance, bool randomizePoint = false)
+        {
+            controller = enemyController;
+            player = playerObj;
+            waypoints = wayPoints;
+            distance = waypointDistance;
+            playerDist = playerDistance;
+            randomPoint = randomizePoint;
+        }
+
+        public override void Act()
+        {
+
+        }
+
+        public override void EnterStateInit()
+        {
+            sawPlayer = false;
+        }
+
+
+        public override void Reason()
+        {
+            //Check distance to waypoint
+            if (Vector3.Distance(controller.transform.position, waypoints[patrolID].transform.position) < distance)
+            {
+                if (randomPoint)
+                {
+                    patrolID = Random.Range(0, waypoints.Length);
+                }
+                else
+                {
+                    patrolID++;
+
+                    if (patrolID > waypoints.Length) patrolID = 0;
+                }
+            }
+
+            //Check distance to player
+            if (Vector3.Distance(controller.transform.position, player.gameObject.transform.position) < playerDist)
+            {
+                if (sawPlayer)
+                {
+                    controller.PerformTransition(Transition.SawPlayer);
+                }
+            }
+        }
+
+        IEnumerator SeesPlayer()
+        {
+            bool leave = false;
+            while (!leave)
+            {
+                yield return new WaitForSeconds(1f);
+                Vector3 dir = player.transform.position - controller.transform.position;
+                Ray ray = new Ray(controller.transform.position, dir);
+                RaycastHit hitInfo;
+
+                Physics.Raycast(ray, out hitInfo);
+
+                if (hitInfo.collider.gameObject.Equals(player.gameObject))
+                {
+                    sawPlayer = true;
+                    leave = true;
+                }
+            }
+        }
+
+        void AvoidObstacles(ref Vector3 dir)
+        {
+            RaycastHit hitInfo;
+
+            //Check direction facing
+            if (Physics.SphereCast(controller.transform.position, controller.raySize, controller.transform.forward.normalized, 
+                out hitInfo, controller.collisionCheckDistance, controller.obstacleLayer) ||
+                Physics.SphereCast(controller.transform.position, controller.raySize, controller.rbSelf.velocity.normalized, 
+                out hitInfo, controller.collisionCheckDistance, controller.obstacleLayer))
+            {
+                // Get the desired direction we need to move to move around  the obstacle. Transform to world co-ordinates (gets the obstacleMoveDirection wrt the current foward direction).
+                Vector3 turnDir = transform.TransformDirection(transform.right);
+                turnDir.Normalize();
+
+                dir += turnDir;
+                obstacleHit = true;
+            }
+        }
+
+
+    }
+}
