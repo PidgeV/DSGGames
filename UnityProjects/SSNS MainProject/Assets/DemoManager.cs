49e9d279547f7ffe947cdba645b3bf2cff55001c
houghtob123 <houghtob123@gmail.com>
2020-01-15 20:06:35 -0500

    Started Demo Manager
    Ran into an issue with unity not allowing drag and drop. Can't test anything or do much

---
+++ UnityProjects/SSNS MainProject/Assets/DemoManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Keybindings:
+/// 
+/// Number keys are to switch between cameras
+/// M to spawn charger enemy
+/// N to spawn fighter enemy
+/// </summary>
+public class DemoManager : MonoBehaviour
+{
+    [SerializeField] Camera[] cameras;
+    [SerializeField] GameObject chargerPrefab;
+    [SerializeField] GameObject fighterPrefab;
+
+    [SerializeField] GameObject playerObj;
+    GameObject spawnedEnemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(Input.GetKeyDown("1") || Input.GetKeyDown("2") || Input.GetKeyDown("3") || Input.GetKeyDown("4") || Input.GetKeyDown("5"))
+        //{
+
+        //}
+        foreach (KeyCode vKey in System.Enum.GetValues(typeof(KeyCode)))
+        {
+            if (Input.GetKey(vKey))
+            {
+                //your code here
+                var test = vKey;
+            }
+        }
+    }
+}
