c606e94e6d672099939f459919cde504a2b6e567
houghtob123 <houghtob123@gmail.com>
2020-01-15 21:20:18 -0500

    Demo Manager finished
    Can handle up to 10 cameras.
    N and M to spawn enemies.
    0-9 for camera changing.

--- UnityProjects/SSNS MainProject/Assets/DemoManager.cs
+++ UnityProjects/SSNS MainProject/Assets/DemoManager.cs
@@ -12,9 +12,12 @@ using UnityEngine;
 /// </summary>
 public class DemoManager : MonoBehaviour
 {
+    Camera enabledCam;
+    [Tooltip("Can use up to 10 cameras")]
     [SerializeField] Camera[] cameras;
     [SerializeField] GameObject chargerPrefab;
     [SerializeField] GameObject fighterPrefab;
+    [SerializeField] Transform enemySpawnLocation;
 
     [SerializeField] GameObject playerObj;
     GameObject spawnedEnemy;
@@ -22,22 +25,77 @@ public class DemoManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        enabledCam = cameras[0];
+        foreach (Camera c in cameras)
+        {
+            c.enabled = false;
+        }
+        enabledCam.enabled = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if(Input.GetKeyDown("1") || Input.GetKeyDown("2") || Input.GetKeyDown("3") || Input.GetKeyDown("4") || Input.GetKeyDown("5"))
-        //{
+        ChangeCamera();
+        SpawnEnemy();
+    }
 
-        //}
+    void ChangeCamera()
+    {
         foreach (KeyCode vKey in System.Enum.GetValues(typeof(KeyCode)))
         {
             if (Input.GetKey(vKey))
             {
                 //your code here
                 var test = vKey;
+
+                if (test.ToString().Length == 6)
+                {
+                    string strTest = test.ToString().Remove(0, 5);
+                    int camNum = int.Parse(strTest);
+
+                    if (camNum >= 0 && camNum < cameras.Length)
+                    {
+                        enabledCam.enabled = false;
+                        enabledCam = cameras[camNum];
+                        enabledCam.enabled = true;
+                    }
+                }
+            }
+        }
+    }
+
+    void SpawnEnemy()
+    {
+
+        if (Input.GetKeyDown(KeyCode.M) && chargerPrefab != null)
+        {
+            if (enemySpawnLocation != null)
+            {
+                if (spawnedEnemy != null)
+                {
+                    Destroy(spawnedEnemy);
+                }
+                spawnedEnemy = Instantiate(chargerPrefab, enemySpawnLocation.position, enemySpawnLocation.rotation);
+            }
+            else
+            {
+                Debug.LogError("Spawn location missing. Please give Demo Manager an enemy spawn location.");
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.N) && fighterPrefab != null)
+        {
+            if (enemySpawnLocation != null)
+            {
+                if (spawnedEnemy != null)
+                {
+                    Destroy(spawnedEnemy);
+                }
+                spawnedEnemy = Instantiate(fighterPrefab, enemySpawnLocation.position, enemySpawnLocation.rotation);
+            }
+            else
+            {
+                Debug.LogError("Spawn location missing. Please give Demo Manager an enemy spawn location.");
             }
         }
     }
