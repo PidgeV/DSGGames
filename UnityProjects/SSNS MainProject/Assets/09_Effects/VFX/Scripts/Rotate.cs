4410926bc565dd086651343d1ac8a218d841ea02
Nathen Toms <45152858+NTomsNC@users.noreply.github.com>
2020-01-28 15:55:16 -0500

    Effects

---
+++ UnityProjects/SSNS MainProject/Assets/09_Effects/VFX/Scripts/Rotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float X, Y, Z;
+    public GameObject pickup;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pickup.transform.Rotate(X, Y, Z, Space.Self);
+    }
+}
