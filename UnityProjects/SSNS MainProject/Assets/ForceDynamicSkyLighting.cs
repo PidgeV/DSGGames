2fdea382f1bd6762e110a767672d96135b77f677
houghtob123 <houghtob123@gmail.com>
2020-01-29 18:55:53 -0500

    Added ForceDynamicSkyLighting
    -Changed StaticSkyLighting to allow dynamic lighting
    -Added ForceDynamicSkyLighting to be paired with StaticSkyLighting to allow skyboxes to dynamically change lighting on change

---
+++ UnityProjects/SSNS MainProject/Assets/ForceDynamicSkyLighting.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.HDPipeline;
+
+[RequireComponent(typeof(StaticLightingSky))]
+public class ForceDynamicSkyLighting : MonoBehaviour
+{
+    private void Awake()
+    {
+        GetComponent<StaticLightingSky>().dynamicLighting = true;
+    }
+}
