3511227bd3d2f33c6d7a7856c96318c5ff217e1a
houghtob123 <33847136+houghtob123@users.noreply.github.com>
2020-02-14 18:58:56 -0500

    Laser look enhancements and sound changes

--- UnityProjects/SSNS MainProject/Assets/05_Scripts/Projectiles/Shots/LaserBehaviour.cs
+++ UnityProjects/SSNS MainProject/Assets/05_Scripts/Projectiles/Shots/LaserBehaviour.cs
@@ -4,14 +4,45 @@ using UnityEngine;
 
 public class LaserBehaviour : MonoBehaviour
 {
+    [SerializeField] LineRenderer laserLine;
+    [SerializeField] ParticleSystemRenderer laserParticle;
     [SerializeField] LineRenderer line;
-    [SerializeField] float laserLength = 400f;
+    [SerializeField] float laserLength = 500f;
     [SerializeField] float laserDamagePerSecond = 5f;
+    public bool fadeIn = false;
 
+    Material[] laserMaterials;
     public float Length { get { return laserLength; } }
     public float Damage { get { return laserDamagePerSecond * Time.deltaTime; } }
+
+    private void Start()
+    {
+        laserMaterials = new Material[2];
+        laserMaterials[0] = laserLine.material;
+        laserMaterials[1] = laserParticle.material;
+
+        StartCoroutine(Fade());
+    }
+
     public void SetLaser(Vector3 endPosition)
     {
         line.SetPosition(1, endPosition);
+    }
+
+    IEnumerator Fade()
+    {
+        while (true)
+        {
+            yield return new WaitForEndOfFrame();
+
+            float fade;
+            if (fadeIn) fade = Mathf.Clamp((laserMaterials[0].GetFloat("Vector1_2C1D604B") - 0.5f * Time.deltaTime), 0.5f, 1);
+            else fade = Mathf.Clamp((laserMaterials[0].GetFloat("Vector1_2C1D604B") + 0.5f * Time.deltaTime), 0.5f, 1);
+
+            foreach (Material m in laserMaterials)
+            {
+                m.SetFloat("Vector1_2C1D604B", fade);
+            }
+        }
     }
 }
--- UnityProjects/SSNS MainProject/Assets/05_Scripts/Sound/Projectiles/LaserSoundController.cs
+++ UnityProjects/SSNS MainProject/Assets/05_Scripts/Sound/Projectiles/LaserSoundController.cs
@@ -6,20 +6,53 @@ using UnityEngine;
 /// This controller will change the audio source of the laser object based on when it is turned on or not.
 /// </summary>
 [RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(LaserBehaviour))]
 public class LaserSoundController : MonoBehaviour
 {
-    [SerializeField] AudioSource startSound;
+    [SerializeField] AudioSource otherSound;
     [SerializeField] AudioSource constantSound;
-    [SerializeField] AudioSource disableSound;
 
-    private void OnEnable()
+    [SerializeField] AudioClip startSound;
+    [SerializeField] AudioClip disableSound;
+
+    LaserBehaviour laser;
+    bool startLaser = false;
+
+    private void Start()
     {
-        if(constantSound) constantSound.Play();
-        if(startSound) startSound.Play();
+        laser = GetComponent<LaserBehaviour>();
+        StartCoroutine(FadeLaserSound());
     }
 
-    private void OnDisable()
+    private void Update()
     {
-        if (disableSound) disableSound.Play();
+        if(laser.fadeIn != startLaser)
+        {
+            startLaser = laser.fadeIn;
+
+            if (startLaser)
+                otherSound.clip = startSound;
+            else
+                otherSound.clip = disableSound;
+
+            otherSound.Play();
+        }
+    }
+
+    IEnumerator FadeLaserSound()
+    {
+
+        while (true)
+        {
+            yield return null;
+            if(startLaser)
+            {
+                constantSound.volume = Mathf.Clamp(constantSound.volume + 0.7f * Time.deltaTime, 0, 1);
+            }
+            else
+            {
+                constantSound.volume = Mathf.Clamp(constantSound.volume - 0.7f * Time.deltaTime, 0, 1);
+            }
+        }
     }
 }
