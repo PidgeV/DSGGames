911797456f2d61968183f5a8b81b085db589267a
Nathen Toms <45152858+NTomsNC@users.noreply.github.com>
2020-02-16 23:12:09 -0500

    Small Fix with jack

--- UnityProjects/SSNS MainProject/Assets/05_Scripts/Player/testShipController.cs
+++ UnityProjects/SSNS MainProject/Assets/05_Scripts/Player/testShipController.cs
@@ -395,7 +395,7 @@ public class testShipController : MonoBehaviour
         {
             // POSITION
             // Where the camera SHOULD be relative to the ship model
-            gunnerCamera.transform.position = ship.transform.position + (ship.transform.up * -4);
+            gunnerCamera.transform.position = ship.transform.position + (ship.transform.up * -6);
 
             // ROTATION
             // Add the current input to the guns final rotation
