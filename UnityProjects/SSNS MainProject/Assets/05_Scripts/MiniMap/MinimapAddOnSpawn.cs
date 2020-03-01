40880dfcd86b65527cf3c077802a422e21aea577
houghtob123 <houghtob123@gmail.com>
2020-03-01 16:07:07 -0500

    dialogue system and minimap system
    -Dialogue system made and implemented. Just give it information when you want things said and it will queue them up
    -Minimap changes: Got rid of lines, started using color for depth instead. Noticed that color doesnt work with the minimap. Should discuss removing minimap from gameplay

---
+++ UnityProjects/SSNS MainProject/Assets/05_Scripts/MiniMap/MinimapAddOnSpawn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapAddOnSpawn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.FindGameObjectWithTag("MinimapCamera").GetComponent<MiniMapDistanceColor>().Add(gameObject);
+    }
+
+    private void OnDestroy()
+    {
+        if (GameObject.FindGameObjectWithTag("MinimapCamera"))
+            GameObject.FindGameObjectWithTag("MinimapCamera").GetComponent<MiniMapDistanceColor>().Remove(gameObject);
+    }
+}
---
+++ UnityProjects/SSNS MainProject/Assets/05_Scripts/Sound/Dialogue/DialogueClass.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Used to store the sound clip and text and keep the data connected
+/// </summary>
+[System.Serializable]
+public class DialogueClass
+{
+    public AudioClip soundClip;
+    public string text;
+}
