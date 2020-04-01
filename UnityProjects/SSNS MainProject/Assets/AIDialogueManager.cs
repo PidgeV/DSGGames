6f1ab4722d66a16d77c4faca0d2fdbb2ab9a1c58
houghtob123 <houghtob123@gmail.com>
2020-03-31 21:35:05 -0400

    Fixed merge and added AI dialogue manager
    -Ai dialogue manager is on the dialogue manager object.
    -Added back main.unity after fucked up merge

---
+++ UnityProjects/SSNS MainProject/Assets/AIDialogueManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIDialogueManager : MonoBehaviour
+{
+    [SerializeField] NumberRange dialogueRange;
+    [SerializeField] NumberRange timeSeconds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (timeSeconds.min >= timeSeconds.max) timeSeconds.min = timeSeconds.max / 2;
+
+        StartCoroutine(RandomizeDialogue());
+    }
+
+    IEnumerator RandomizeDialogue()
+    {
+        while (true)
+        {
+            float rand = Random.Range(timeSeconds.min, timeSeconds.max);
+
+            yield return new WaitForSecondsRealtime(rand);
+
+            int index = Random.Range((int)dialogueRange.min, (int)dialogueRange.max + 1);
+            DialogueSystem.Instance.AddDialogue(index);
+        }
+    }
+}
