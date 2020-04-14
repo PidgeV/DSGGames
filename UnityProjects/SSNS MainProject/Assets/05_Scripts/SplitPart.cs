bfdf29864f88d51df6d711e4b70bd5b8aa57a506
DestELYK <kd_master@live.ca>
2020-04-14 00:24:55 -0400

    Add Derek's destroyed player ship to the player death

---
+++ UnityProjects/SSNS MainProject/Assets/05_Scripts/SplitPart.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.VFX;
+
+public class SplitPart : MonoBehaviour
+{
+    [SerializeField] private GameObject partsParent;
+    [SerializeField] private float speed = 1000;
+
+    private void Awake()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Transform part in partsParent.transform)
+        {
+            Vector3 dir = (part.transform.position - partsParent.transform.position).normalized;
+
+            dir += Random.insideUnitSphere * 10;
+
+            dir = dir.normalized;
+
+            Vector3 rotation = Random.insideUnitSphere * speed;
+
+            if (part.TryGetComponent(out Rigidbody rigid))
+            {
+                rigid.AddForce(dir * Random.Range(speed / 1.3f, speed), ForceMode.VelocityChange);
+
+                rigid.AddTorque(rotation, ForceMode.VelocityChange);
+            }
+
+            if (part.TryGetComponent(out VisualEffect vfx))
+            {
+                vfx.SetVector3("Force Amount", -dir);
+            }
+        }
+    }
+}
