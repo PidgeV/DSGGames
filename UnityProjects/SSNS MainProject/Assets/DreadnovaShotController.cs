a85e13440c8c09001e47f4acd5a6c14a33ef3796
Blake <houghtob123@gmail.com>
2020-05-10 19:59:35 -0400

    stuff

---
+++ UnityProjects/SSNS MainProject/Assets/DreadnovaShotController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DreadnovaShotController : MonoBehaviour
+{
+    [SerializeField] GameObject ChargeEffect;
+    [SerializeField] GameObject ChargeShot;
+    [SerializeField] float startTime;
+    [SerializeField] float chargeDelay = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(StartShot());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator StartShot()
+    {
+        if (startTime > 0) yield return new WaitForSeconds(startTime);
+
+        ChargeEffect.SetActive(true);
+
+        yield return new WaitForSeconds(chargeDelay);
+
+        ChargeShot.SetActive(true);
+    }
+}
---
+++ UnityProjects/SSNS MainProject/Assets/TriggerAnim.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class TriggerAnim : MonoBehaviour
+{
+    [SerializeField] float triggerTime = 2f;
+
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+        StartCoroutine(TriggerAnimation());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator TriggerAnimation()
+    {
+        yield return new WaitForSecondsRealtime(triggerTime);
+
+        anim.SetTrigger("Start");
+    }
+}
