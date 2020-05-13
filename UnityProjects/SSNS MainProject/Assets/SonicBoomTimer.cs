5a68b68a54933dc3dea5c973258475c31776f9de
Blake <houghtob123@gmail.com>
2020-05-12 20:53:21 -0400

    SonicBoomTimer

---
+++ UnityProjects/SSNS MainProject/Assets/SonicBoomTimer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SonicBoomTimer : MonoBehaviour
+{
+    [SerializeField] float[] frames;
+    [SerializeField] GameObject SonicBoomPrefab;
+
+    float count = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(float f in frames)
+        {
+            if (count == f)
+            {
+                Instantiate(SonicBoomPrefab, transform);
+                Debug.Log("Sonic Boom Spawned");
+            }
+        }
+
+        count++;
+        Debug.Log("Frame: " + count);
+    }
+}
